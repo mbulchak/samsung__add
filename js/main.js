9f4176513727ed21f3722e83112244c607ce1c79
Mariia Bulchak <bulchakmariiast@gmail.com>
2025-01-31 13:47:01 +0200

    final changes about pagination and changing text and image

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ function newTab() {
   window.open("empty.html", "_blank");
 }
 
+gsap.registerPlugin(TextPlugin);
+
+var secTl = gsap.timeline();
+
+var mainTl = gsap.timeline();
+
 gsap.set(".logo__main", {
   opacity: 0,
   x: 0,
@@ -61,11 +67,13 @@ gsap.to(".frame", {
   background: "none",
 });
 
-gsap.set(".second__part", {
+// second__part
+
+secTl.set(".second__part", {
   background: "none",
 });
 
-gsap.to(".second__part", {
+secTl.to(".second__part", {
   delay: 3.5,
   duration: 3,
   backgroundImage: "url(./../images/main-screen.png)",
@@ -75,25 +83,25 @@ gsap.to(".second__part", {
   backgroundPositionX: "80%",
 });
 
-gsap.to(".second__part", {
-  delay: 8.3,
+secTl.to(".second__part", {
+  delay: 2,
   duration: 0,
   backgroundImage: "url(./../images/frame__2.png)",
 });
 
-gsap.to(".second__part", {
-  delay: 11.3,
+secTl.to(".second__part", {
+  delay: 3,
   duration: 0,
   backgroundImage: "url(./../images/frame__3.png)",
 });
 
-gsap.to(".second__part", {
-  delay: 14.3,
+secTl.to(".second__part", {
+  delay: 3,
   duration: 0,
   backgroundImage: "url(./../images/frame__4.jpg)",
 });
-gsap.to(".second__part", {
-  delay: 17.3,
+secTl.to(".second__part", {
+  delay: 3,
   duration: 0,
   backgroundImage: "url(./../images/frame__5.png)",
 });
@@ -108,85 +116,206 @@ gsap.to(".description", {
   duration: 2,
 });
 
-gsap.registerPlugin(TextPlugin);
-
 var tl = gsap.timeline();
 
 const descr = document.querySelector(".description__main");
+const arrowLeft = document.querySelector('.arrow-left');
+const arrowRight = document.querySelector('.arrow-right');
 
-gsap.to(descr, {
+const texts = [
+  "“The first time I used the Samsung Bespoke Jet™, I cried. I’m not being sensational; I really did. Of course, this vacuum worked great. But that’s not all.”",
+  "“If you’re an over-cleaner, like myself, you’ll nerd \n out on all of the functions. If you avoid this chore \n at all costs, you’ll appreciate how simple \n Samsung makes it.”",
+  "“Both the floor and pet hair attachments are cleverly designed to eliminate the dreaded hair wrap. (In other words, you’ll never have to tackle hair tangles with a pair of scissors again.)”",
+  "“When I learned the Samsung Bespoke Vac cleaned itself with amazing technology, that’s when I cried. No more scraping spider legs and hair out of the crevices with my hands. Its suction power is so strong, the canister is left perfectly clean after every use. It’s like a vacuum for your vacuum.”",
+  "“Because it’s so nice-looking, it can live right in the kitchen. No more hauling a vacuum up and down the basement stairs on the daily”",
+];
+
+const images = [
+  "url(./../images/main-screen.png)",
+  "url(./../images/frame__2.png)",
+  "url(./../images/frame__3.png)",
+  "url(./../images/frame__4.jpg)",
+  "url(./../images/frame__5.png)",
+];
+
+let currentIndex = 0;
+let time = 5.3;
+
+tl.to(descr, {
   duration: 0,
   delay: 5.3,
   text: {
-    value:
-      "“The first time I used the Samsung Bespoke Jet™, I cried. I’m not being sensational; I really did. Of course, this vacuum worked great. But that’s not all.”",
+    value: texts[0],
     delimiter: "  ",
+  },
+  onStart: () => {
+      currentIndex = 0;
+    },
+});
+
+tl.to(descr, {
+  x: 10,
+  duration: 1,
+  delay: 2.8,
+  text: {
+    value: texts[1],
+    delimiter: "  ",
+  },
+  onStart: () => {
+      currentIndex = 1;
+    },
+  onComplete: () => {
+    gsap.to(descr, {
+      x: 0,
+    });
   },
 });
 
-gsap.to(descr, {
+tl.to(descr, {
   x: 10,
   duration: 1,
+  delay: 2.5,
+  text: {
+    value: texts[2],
+    delimiter: "  ",
+  },
+  onStart: () => {
+      currentIndex = 2;
+    },
+  onComplete: () => {
+    gsap.to(descr, {
+      x: 0,
+    });
+  },
+});
+
+tl.to(descr, {
+  x: 10,
+  duration: 1,
+  delay: 1.8,
+  text: {
+    value: texts[3],
+    delimiter: "  ",
+  },
+  onStart: () => {
+      currentIndex = 3;
+    },
+  onComplete: () => {
+    gsap.to(descr, {
+      x: 0,
+    });
+  },
+});
+
+tl.to(descr, {
+  x: 10,
+  duration: 1,
+  delay: 2,
+  text: {
+    value: texts[4],
+    delimiter: "  ",
+  },
+  onStart: () => {
+      currentIndex = 4;
+    },
+  onComplete: () => {
+    gsap.to(descr, {
+      x: 0,
+    });
+  },
+}); 
+
+// arrow
+
+const arrTl = gsap.timeline();
+
+arrTl.to(".arrow", {
+  duration: 2,
+  text: "2/5",
   delay: 8.3,
-  text: {
-    value:
-      "“If you’re an over-cleaner, like myself, you’ll nerd \n out on all of the functions. If you avoid this chore \n at all costs, you’ll appreciate how simple \n Samsung makes it.”",
-    delimiter: "  ",
-  },
-  onComplete: () => {
-    gsap.to(descr, {
-      x: 0,
-    });
-  },
 });
 
-gsap.to(descr, {
-  x: 10,
-  duration: 1,
-  delay: 11.3,
-  text: {
-    value:
-      "“Both the floor and pet hair attachments are cleverly designed to eliminate the dreaded hair wrap. (In other words, you’ll never have to tackle hair tangles with a pair of scissors again.)” ",
-    delimiter: "  ",
-  },
-  onComplete: () => {
-    gsap.to(descr, {
-      x: 0,
-    });
-  },
+arrTl.to(".arrow", {
+  duration: 2,
+  text: "3/5",
+  delay: 1.2,
 });
 
-gsap.to(descr, {
-  x: 10,
-  duration: 1,
-  delay: 14.3,
-  text: {
-    value:
-      "“When I learned the Samsung Bespoke Vac cleaned itself with amazing technology, that’s when I cried. No more scraping spider legs and hair out of the crevices with my hands. Its suction power is so strong, the canister is left perfectly clean after every use. It’s like a vacuum for your vacuum.” ",
-    delimiter: "  ",
-  },
-  onComplete: () => {
-    gsap.to(descr, {
-      x: 0,
-    });
-  },
+arrTl.to(".arrow", {
+  duration: 2,
+  text: "4/5",
+  delay: 1,
 });
 
-gsap.to(descr, {
-  x: 10,
-  duration: 1,
-  delay: 17.3,
-  text: {
-    value:
-      "“Because it’s so nice-looking, it can live right in the kitchen. No more hauling a vacuum up and down the basement stairs on the daily”",
-    delimiter: "  ",
-  },
-  onComplete: () => {
-    gsap.to(descr, {
-      x: 0,
-    });
-  },
+arrTl.to(".arrow", {
+  duration: 2,
+  text: "5/5",
+  delay: 1,
 });
+
+arrowLeft.addEventListener('click', () => {
+  console.log('arrrrrrr')
+  navigateContent(-1);
+})
+arrowRight.addEventListener('click', () => {
+  console.log('arrrrrrr453');
+  navigateContent(1);
+})
+
+let resumeTimeout;
+
+function navigateContent(direction) {
+  clearTimeout(resumeTimeout);
+  tl.pause();
+  secTl.pause();
+  arrTl.pause();
+
+
+  currentIndex += direction;
+
+  if (currentIndex < 0) {
+    currentIndex = texts.length - 1;
+  } else if (currentIndex >= texts.length) {
+    currentIndex = 0;
+  }
+
+  // continue
+  updateAll();
+
+  resumeTimeout = setTimeout(() => {
+    let newTime = 5.3 + currentIndex * 3;
+    tl.seek(newTime);
+    secTl.seek(newTime);
+    arrTl.seek(newTime);
+
+    tl.play();
+    secTl.play();
+    arrTl.play();
+  }, 3000)
+}
+
+function updateAll() {
+  gsap.killTweensOf(descr, ".second__part", '.arrow');
+
+  gsap.to(descr, {
+    duration: 0,
+    text: {
+      value: texts[currentIndex],
+
+      delimiter: '  ',
+    }, ease: "power2.out",
+  })
+
+  gsap.to(".second__part", {
+    duration: 0,
+    backgroundImage: images[currentIndex],
+  })
+
+  gsap.to('.arrow', {
+    duration: 0,
+    text: `${currentIndex + 1}/5`
+  })
+}
 
 gsap.set(".quote", {
   marginBottom: 115,
@@ -210,30 +339,6 @@ gsap.to(".count", {
   opacity: 1,
   delay: 5.3,
   duration: 2,
-});
-
-gsap.to(".arrow", {
-  duration: 2,
-  text: "2/5",
-  delay: 8.3,
-});
-
-gsap.to(".arrow", {
-  duration: 2,
-  text: "3/5",
-  delay: 11.3,
-});
-
-gsap.to(".arrow", {
-  duration: 2,
-  text: "4/5",
-  delay: 14.3,
-});
-
-gsap.to(".arrow", {
-  duration: 2,
-  text: "5/5",
-  delay: 17.3,
 });
 
 // button__shop and hover effect
@@ -266,7 +371,7 @@ shop__button.addEventListener("mouseenter", () => {
   gsap.to(shop__button, {
     scale: 1.3,
     duration: 2,
-    ease: "power2.out"
+    ease: "power2.out",
   });
 });
 shop__button.addEventListener("mouseout", () => {
